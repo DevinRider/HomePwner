acccf258938698454bee652a52a0971fec94d84d
Devin Rider <devinrrider@gmail.com>
2014-06-09 08:59:51 -0700

    Initial Commit

---
+++ Homepwner/DEVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DEVAppDelegate.h
+//  Homepwner
+//
+//  Created by Devin on 6/9/14.
+//  Copyright (c) 2014 Devin Rider. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DEVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
